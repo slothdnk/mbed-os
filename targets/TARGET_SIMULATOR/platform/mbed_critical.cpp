a4c927082b3cf7f2cd71013f1447e49be0e33074
Jan Jongboom <janjongboom@gmail.com>
2019-01-24 14:40:53 -0800

    Add new core_util functions

--- targets/TARGET_SIMULATOR/platform/mbed_critical.cpp
+++ targets/TARGET_SIMULATOR/platform/mbed_critical.cpp
@@ -10,4 +10,27 @@ bool core_util_is_isr_active(void) {
 
 void core_util_critical_section_enter(void) {}
 
-void core_util_critical_section_exit(void) {}
+/**
+ * Atomic test and set.
+ *
+ * Atomically tests then sets the flag to true, returning the previous value.
+ *
+ * @param  flagPtr Target flag being tested and set.
+ * @return         The previous value.
+ */
+bool core_util_atomic_flag_test_and_set(volatile core_util_atomic_flag *flagPtr) {
+    bool curr = flagPtr->_flag;
+    flagPtr->_flag = true;
+    return true;
+}
+
+/**
+ * Atomic clear.
+ *
+ * @param  flagPtr Target flag being cleared.
+ */
+void core_util_atomic_flag_clear(volatile core_util_atomic_flag *flagPtr) {
+    flagPtr->_flag = false;
+}
+
+
